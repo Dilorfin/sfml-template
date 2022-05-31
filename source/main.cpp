bb0efe055e0144f6579a3f5ce9732eae9a9a6c08
Dilorfin <pavlo_kuibida@epam.com>
2022-05-31 21:26:36 +0300

    Fix for SFML 3.0

--- source/main.cpp
+++ source/main.cpp
@@ -1,76 +1,6 @@
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
-
-// Do we want to showcase direct JNI/NDK interaction?
-// Undefine this to get real cross-platform code.
-// Uncomment this to try JNI access; this seems to be broken in latest NDKs
-//#define USE_JNI
-
-#if defined(USE_JNI)
-// These headers are only needed for direct NDK/JDK interaction
-#include <jni.h>
-#include <android/native_activity.h>
-
-// Since we want to get the native activity from SFML, we'll have to use an
-// extra header here:
-#include <SFML/System/NativeActivity.hpp>
-
-// NDK/JNI sub example - call Java code from native code
-int vibrate(sf::Time duration)
-{
-    // First we'll need the native activity handle
-    ANativeActivity *activity = sf::getNativeActivity();
-
-    // Retrieve the JVM and JNI environment
-    JavaVM* vm = activity->vm;
-    JNIEnv* env = activity->env;
-
-    // First, attach this thread to the main thread
-    JavaVMAttachArgs attachargs;
-    attachargs.version = JNI_VERSION_1_6;
-    attachargs.name = "NativeThread";
-    attachargs.group = nullptr;
-    jint res = vm->AttachCurrentThread(&env, &attachargs);
-
-    if (res == JNI_ERR)
-        return EXIT_FAILURE;
-
-    // Retrieve class information
-    jclass natact = env->FindClass("android/app/NativeActivity");
-    jclass context = env->FindClass("android/content/Context");
-
-    // Get the value of a constant
-    jfieldID fid = env->GetStaticFieldID(context, "VIBRATOR_SERVICE", "Ljava/lang/String;");
-    jobject svcstr = env->GetStaticObjectField(context, fid);
-
-    // Get the method 'getSystemService' and call it
-    jmethodID getss = env->GetMethodID(natact, "getSystemService", "(Ljava/lang/String;)Ljava/lang/Object;");
-    jobject vib_obj = env->CallObjectMethod(activity->clazz, getss, svcstr);
-
-    // Get the object's class and retrieve the member name
-    jclass vib_cls = env->GetObjectClass(vib_obj);
-    jmethodID vibrate = env->GetMethodID(vib_cls, "vibrate", "(J)V");
-
-    // Determine the timeframe
-    jlong length = duration.asMilliseconds();
-
-    // Bzzz!
-    env->CallVoidMethod(vib_obj, vibrate, length);
-
-    // Free references
-    env->DeleteLocalRef(vib_obj);
-    env->DeleteLocalRef(vib_cls);
-    env->DeleteLocalRef(svcstr);
-    env->DeleteLocalRef(context);
-    env->DeleteLocalRef(natact);
-
-    // Detach thread again
-    vm->DetachCurrentThread();
-}
-#endif
 
 // This is the actual Android example. You don't have to write any platform
 // specific code, unless you want to use things not directly exposed.
@@ -87,7 +17,8 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
 
     sf::Sprite image(texture);
-    image.setPosition({screen.width / 2.f, screen.height / 2.f});
+
+    image.setPosition({screen.size.x / 2.f, screen.size.y / 2.f});
     image.setOrigin({texture.getSize().x / 2.f, texture.getSize().y / 2.f});
 
     sf::Font font;
@@ -123,8 +54,8 @@ int main(int argc, char *argv[])
                         window.close();
                     break;
                 case sf::Event::Resized:
-                    view.setSize(event.size.width, event.size.height);
-                    view.setCenter({event.size.width / 2.f, event.size.height / 2.f});
+                    view.setSize(sf::Vector2f(event.size.width, event.size.height));
+                    view.setCenter(sf::Vector2f(event.size.width / 2.f, event.size.height / 2.f));
                     window.setView(view);
                     break;
                 case sf::Event::LostFocus:
@@ -145,10 +76,7 @@ int main(int argc, char *argv[])
                 case sf::Event::TouchBegan:
                     if (event.touch.finger == 0)
                     {
-                        image.setPosition({static_cast<float>(event.touch.x), static_cast<float>(event.touch.y)});
-#if defined(USE_JNI)
-                        vibrate(sf::milliseconds(10));
-#endif
+                        image.setPosition(sf::Vector2f(static_cast<float>(event.touch.x), static_cast<float>(event.touch.y)));
                     }
                     break;
             }
