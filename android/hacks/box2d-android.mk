d7161792492b41a88a12c09667a4e988f786c9c0
Dilorfin <pavlo_kuibida@epam.com>
2022-06-01 18:30:08 +0300

    Add box2d Android.mk

---
+++ android/hacks/box2d-android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := box2d
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libbox2d.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+include $(PREBUILT_SHARED_LIBRARY)
