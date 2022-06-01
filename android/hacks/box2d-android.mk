8036680ce5524ae85f588fa510f4983f8c9f7df0
Dilorfin <pavlo_kuibida@epam.com>
2022-06-01 20:36:12 +0300

    Config github workflow 0

--- android/hacks/box2d-android.mk
+++ android/hacks/box2d-android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := box2d
-LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libbox2d.so
+LOCAL_SRC_FILES := lib/libbox2d.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 
 include $(PREBUILT_SHARED_LIBRARY)
