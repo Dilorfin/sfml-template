e89e8fd84de430ff485b8b2c133a8b1caeab18c1
Dilorfin <pavlo_kuibida@epam.com>
2022-07-27 17:21:38 +0300

    Fix formatting

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,7 +1,10 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
-int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+int factorial(int number)
+{
+	return number <= 1 ? number : factorial(number - 1) * number;
+}
 
 TEST_CASE("testing the factorial function")
 {
